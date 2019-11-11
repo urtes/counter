e3ade59d7af89932168cdf0f018b1fd2ecbd6bca
Urte <urte.rimsaite@gmail.com>
2019-11-11 18:05:43 +0200

    Add word exclude counting

--- src/main/java/com/urte/counter/WordCountingService.java
+++ src/main/java/com/urte/counter/WordCountingService.java
@@ -6,17 +6,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WordCountingService {
 
     private final String PATH;
     private final List<File> INPUT_FILES;
+    final String EXCLUDE_FILENAME = "exclude.txt";
+    final String EXCLUDE_COUNT_FILENAME = "exclude_count.txt";
     private Map<String, Long> words = new TreeMap<>();
+    private Set<String> wordsToExclude = new HashSet<>();
+    private long excludeCounter = 0;
 
     public WordCountingService(List<File> inputFiles, String path) {
         this.INPUT_FILES = inputFiles;
@@ -25,12 +27,12 @@ public class WordCountingService {
 
     public void generateWordCountReports() {
         countWordsInFiles();
-        outputReports();
+        generateReports();
     }
 
     private void countWordsInFiles() {
         INPUT_FILES.forEach(file -> countWordsInFile(file));
-        words.entrySet().forEach(count -> System.out.println(count.getKey() + ": " + count.getValue()));
+//        words.entrySet().forEach(count -> System.out.println(count.getKey() + ": " + count.getValue()));
     }
 
     private void countWordsInFile(File file) {
@@ -44,24 +46,36 @@ public class WordCountingService {
     }
 
     private void countWordsInLine(String[] wordsInLine) {
+
+        setExcludes();
+
         for (int i = 0; i < wordsInLine.length; i++) {
-            if (!words.containsKey(wordsInLine[i])) {
-                words.put(wordsInLine[i], 1l);
+            String word = wordsInLine[i];
+            if (wordsToExclude.contains(word)) {
+                excludeCounter++;
             } else {
-                long count = words.get(wordsInLine[i]);
-                words.put(wordsInLine[i], count+1l);
+                if (!words.containsKey(word)) {
+                    words.put(wordsInLine[i], 1l);
+                } else {
+                    long count = words.get(wordsInLine[i]);
+                    words.put(wordsInLine[i], count + 1l);
+                }
             }
         }
     }
 
-    private void outputReports() {
+    private void generateReports() {
+        generateCountReports();
+        generateExcludesReport();
+    }
+
+    private void generateCountReports() {
 
         File file = new File(PATH + "output.txt");
         file.getParentFile().mkdirs();
         PrintWriter printWriter = null;
 
         try {
-            file.getParentFile().mkdirs();
             printWriter = new PrintWriter(file);
             for(Map.Entry<String, Long> word : words.entrySet()) {
                 printWriter.println(String.format("%s: %d", word.getKey(), word.getValue()));
@@ -74,4 +88,44 @@ public class WordCountingService {
             }
         }
     }
+
+    public void generateExcludesReport() {
+        File file = new File(PATH + EXCLUDE_COUNT_FILENAME);
+        file.getParentFile().mkdirs();
+        PrintWriter printWriter = null;
+
+        try {
+            printWriter = new PrintWriter(file);
+            printWriter.println(String.format("Number of excluded words %d", excludeCounter));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        }
+    }
+
+    private void setExcludes() {
+        File exclude = new File(PATH + EXCLUDE_FILENAME);
+        if(exclude.exists()) {
+            wordsToExclude = getWordsToExclude(exclude);
+        }
+    }
+
+    private Set<String> getWordsToExclude(File exclude) {
+
+        Set<String> wordsToExclude = new HashSet<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(exclude.getPath()))) {
+            wordsToExclude = stream
+                    .map(line -> line.split("[\\s\\p{Punct}]+"))
+                    .flatMap(array -> Arrays.stream(array))
+                    .collect(Collectors.toSet());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordsToExclude;
+    }
+
 }
